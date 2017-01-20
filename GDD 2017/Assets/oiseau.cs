420727e386ba54356d3e8a37a39e66d9c8ae6437
PIGEONCTATURE\aurélien <pigeonfoutu@gmail.com>
2017-01-20 21:16:50 +0100

    premier truc unity

---
+++ GDD 2017/Assets/oiseau.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class oiseau : MonoBehaviour 
+{
+	float vitesse = 0.1f;
+	int x = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate (0,vitesse*x,0);
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.Rotate(0,0,1);
+			x++;
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Rotate(0,0,-1);
+			x--;
+		}
+	
+	}
+
+
+}
