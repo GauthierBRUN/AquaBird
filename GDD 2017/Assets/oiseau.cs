cec01d57f356f68be5be654a8f69a8877b7e5f67
DESKTOP-KV8SGF3\brung <brun.gauthier@gmail.com>
2017-01-20 22:36:49 +0100

    L'oiseau fonctionne presque bien

--- GDD 2017/Assets/oiseau.cs
+++ GDD 2017/Assets/oiseau.cs
@@ -3,32 +3,41 @@ using System.Collections;
 
 public class oiseau : MonoBehaviour 
 {
-	float vitesse = 0.1f;
-	int x = 0;
 
-	// Use this for initialization
-	void Start () 
-	{
+    private static readonly float Vitesse = 0.1f;
 
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		transform.Translate (0,vitesse*x,0);
-		if (Input.GetKey(KeyCode.DownArrow))
-		{
-			transform.Rotate(0,0,1);
-			x++;
-		}
-		if (Input.GetKey(KeyCode.UpArrow))
-		{
-			transform.Rotate(0,0,-1);
-			x--;
-		}
-	
-	}
 
+    private Rigidbody myBody;
+
+    // Use this for initialization
+    void Start () 
+    {
+        myBody = this.GetComponent<Rigidbody>();
+    }
+    
+    // Update is called once per frame
+    void Update () 
+    {
+        Vector3 transalation = Vitesse * InLocalBase(new Vector3(0, Mathf.Sin(transform.rotation.z), 0));
+        //transform.Translate(transalation);
+
+        myBody.AddForce(transalation);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(0,0,1);            
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(0,0,-1);            
+        }
+    
+    }
+
+    Vector3 InLocalBase(Vector3 v)
+    {
+        var alpha = transform.rotation.z;
+        return new Vector3(v.x*Mathf.Cos(alpha) + v.y*Mathf.Sin(alpha), v.x * Mathf.Sin(alpha) + v.y * Mathf.Cos(alpha), 0);
+    }
 
 }
