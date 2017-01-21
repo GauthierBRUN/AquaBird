d06d55900ae704d7f960833c53234ef0ff6f6611
DESKTOP-KV8SGF3\brung <brun.gauthier@gmail.com>
2017-01-21 19:08:58 +0100

    Mocing ground

---
+++ Assets/Code/DecordSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecordSpawner : MonoBehaviour
+{
+    public MovingDecord DecordModel;
+    public float DecordSpeed;
+    public Vector3 SpawnLocation;
+    public float SpawnInterval;
+
+    private float TimeToNextSpawn;
+
+	// Use this for initialization
+	void Start () {
+        Spawn();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        TimeToNextSpawn -= Time.deltaTime;
+
+        if (TimeToNextSpawn < 0)
+        {
+            Spawn();
+        }
+	}
+
+    void Spawn()
+    {
+        var newDecord = Instantiate(DecordModel);
+        newDecord.transform.position = SpawnLocation;
+        newDecord.Speed = DecordSpeed;
+        TimeToNextSpawn = SpawnInterval;
+    }
+
+    
+
+}
