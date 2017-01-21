072a131afda41f1c6cc32a95dd3840854ffe0b2a
Adrien <adrienlemoine35@hotmail.fr>
2017-01-21 12:39:40 +0100

    Ajout des fichiers du projet

---
+++ Assets/Score.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gauthier
+{
+    public class Score : MonoBehaviour
+    {
+        public Text ScoreText;
+        private float ScoreCoefficient;
+        public float FixedUpdateScoreCoeffIncrement;
+        public float ScoreValue;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            ScoreText.text = ScoreValue + " pts";
+        }
+
+        private void FixedUpdate()
+        {
+            ScoreCoefficient += FixedUpdateScoreCoeffIncrement;
+            ScoreValue += ScoreCoefficient * Time.fixedDeltaTime;
+        }
+    }
+}
